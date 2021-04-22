873872de2511933874bb380b468f9a0765e2250a
HerbertSousa <herberti1405@outlook.com>
2021-04-22 11:32:15 -0300

    primic preview

---
+++ src/pages/api/preview.ts
@@ -0,0 +1,33 @@
+/* eslint-disable @typescript-eslint/explicit-function-return-type */
+/* eslint-disable consistent-return */
+/* eslint-disable import/extensions */
+/* eslint-disable import/no-unresolved */
+import { Document } from '@prismicio/client/types/documents';
+import { getPrismicClient } from '../../services/prismic';
+
+export default async (req, res) => {
+  function linkResolver(doc: Document): string {
+    if (doc.type === 'posts') {
+      return `/post/${doc.uid}`;
+    }
+    return '/';
+  }
+
+  const { token: ref, documentId } = req.query;
+  const redirectUrl = await getPrismicClient(req)
+    .getPreviewResolver(ref, documentId)
+    .resolve(linkResolver, '/');
+
+  if (!redirectUrl) {
+    return res.status(401).json({ message: 'Invalid token' });
+  }
+
+  res.setPreviewData({ ref });
+
+  res.write(
+    `<!DOCTYPE html><html><head><meta http-equiv="Refresh" content="0; url=${redirectUrl}" />
+    <script>window.location.href = '${redirectUrl}'</script>
+    </head>`
+  );
+  res.end();
+};
--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -28,9 +28,13 @@ interface PostPagination {
 
 interface HomeProps {
   postsPagination: PostPagination;
+  preview: boolean;
 }
 
-export default function Home({ postsPagination }: HomeProps): JSX.Element {
+export default function Home({
+  postsPagination,
+  preview,
+}: HomeProps): JSX.Element {
   const [posts, setPosts] = useState(postsPagination.results);
   const [nextPage, setNextPage] = useState(postsPagination.next_page);
   const [currentPage, setCurrentPage] = useState();
@@ -110,17 +114,28 @@ export default function Home({ postsPagination }: HomeProps): JSX.Element {
           )}
         </main>
       </div>
+      {preview && (
+        <aside>
+          <Link href="/api/exit-preview">
+            <a>Sair do modo Preview</a>
+          </Link>
+        </aside>
+      )}
     </>
   );
 }
 
-export const getStaticProps: GetStaticProps = async () => {
+export const getStaticProps: GetStaticProps = async ({
+  preview = false,
+  previewData,
+}) => {
   const prismic = getPrismicClient();
   const postsResponse = await prismic.query(
     [Prismic.predicates.at('document.type', 'desafios')],
     {
       fetch: ['desafios.title', 'desafios.subtitle', 'desafios.author'],
       pageSize: 5,
+      ref: previewData?.ref ?? null,
     }
   );
 
@@ -145,6 +160,7 @@ export const getStaticProps: GetStaticProps = async () => {
   return {
     props: {
       postsPagination,
+      preview,
     },
     revalidate: 60 * 30,
   };
